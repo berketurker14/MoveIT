66b6bfe4de9af3c32a534b906c19a845f0539f62
berketurker <45368526+berkosko5000@users.noreply.github.com>
2023-03-12 05:16:21 +0300

    Merge

---
+++ Assets/Witch.cs
@@ -0,0 +1,34 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Witch : MonoBehaviour
+{
+    public GameObject panel;
+    private bool isPanelOpen = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Selam");
+        //if (collision.gameObject.tag == "Player")
+        //{
+            OpenClosePanel(!isPanelOpen);
+        //}
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //if (collision.gameObject.tag == "Player")
+        //{
+            OpenClosePanel(isPanelOpen);
+        //}
+    }
+    void OpenClosePanel(bool test)
+    {
+        panel.SetActive(test);
+    }
+}
