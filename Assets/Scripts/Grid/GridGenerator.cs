95883cd5b549e08c827f34ec71c7e5ec55ef11cf
Muhammed Enes Ünal <menes_unal@hotmail.com>
2023-03-12 03:23:49 +0300

    Grid Generator, Controller, Action Controller done

---
+++ Assets/Scripts/Grid/GridGenerator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    public GameObject spritePrefab;
+    public float width = 30;
+    public float height = 10;
+    public float spacing = 1;
+
+    void Start()
+    {
+        Vector2 offset = new Vector2((width - 1) * spacing / 2f, (height - 1) * spacing / 2f);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Vector2 position = new Vector2(x * spacing, y * spacing) - offset;
+                Instantiate(spritePrefab, transform.position + (Vector3)position, Quaternion.identity, transform);
+            }
+        }
+    }
+}
