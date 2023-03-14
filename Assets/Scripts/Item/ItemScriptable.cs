0dc280d3c7e9ffe22eea1a03457f144b84a8bf05
berketurker <45368526+berkosko5000@users.noreply.github.com>
2023-03-14 23:07:13 +0300

    berke

--- Assets/Scripts/Item/ItemScriptable.cs
+++ Assets/Scripts/Item/ItemScriptable.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemScriptable : MonoBehaviour
+[CreateAssetMenu(fileName = "Item", menuName = "AddItem")]
+public class ItemScriptable : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Sprite item;// prefab => skills?
+    //public Skillset cartcurt;?
+    public Sprite effect;
+    public string name, definition;
+    // rarity is determined by a random value between and if it is between current
+    // intervals f.e. 0-100 is 9.99 percent chance of a common item's in the
+    // scriptableobject in project 
+    [HideInInspector] public enum Rarity { Common, Uncommon, Rare, Epic, Legendary };
+    public Rarity rarity;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
